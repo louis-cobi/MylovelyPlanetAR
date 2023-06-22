4e890b87247d7cb8120394c63609140e3ffe2e2d
louis cobigo <louis.cobigo@gmail.com>
2023-06-22 14:14:54 +0200

    uncomment redirection on click

--- event.js
+++ event.js
@@ -1,25 +1,25 @@
-// AFRAME.registerComponent("markerhandler", {
-//     init: function () {
-//         this.el.addEventListener("click", function (ev, target) {
-//             console.log("edward clicked")
-//             const userAgent =
-//                 navigator.userAgent || navigator.vendor || window.opera
+AFRAME.registerComponent("markerhandler", {
+    init: function () {
+        this.el.addEventListener("click", function (ev, target) {
+            console.log("edward clicked")
+            const userAgent =
+                navigator.userAgent || navigator.vendor || window.opera
 
-//             if (
-//                 (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) ||
-//                 navigator.userAgent.includes("Mac")
-//             ) {
-//                 // Redirection vers l'App Store pour les appareils Apple (iOS)
-//                 window.location.href =
-//                     "https://apps.apple.com/fr/app/my-lovely-planet/id1573748823"
-//             } else if (/android/i.test(userAgent)) {
-//                 // Redirection vers le Play Store pour les appareils Android
-//                 window.location.href =
-//                     "https://play.google.com/store/apps/details?id=com.ImagineComp.ImagineSerie&hl=fr&gl=US"
-//             }
-//         })
-//     },
-// })
+            if (
+                (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) ||
+                navigator.userAgent.includes("Mac")
+            ) {
+                // Redirection vers l'App Store pour les appareils Apple (iOS)
+                window.location.href =
+                    "https://apps.apple.com/fr/app/my-lovely-planet/id1573748823"
+            } else if (/android/i.test(userAgent)) {
+                // Redirection vers le Play Store pour les appareils Android
+                window.location.href =
+                    "https://play.google.com/store/apps/details?id=com.ImagineComp.ImagineSerie&hl=fr&gl=US"
+            }
+        })
+    },
+})
 
 AFRAME.registerComponent("markerhandler", {
     init: function () {
